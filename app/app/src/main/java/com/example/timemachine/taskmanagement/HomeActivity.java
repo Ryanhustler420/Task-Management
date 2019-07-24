32687a8533cc4ae3d63ce30c549fa83167cd62c9
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-24 20:49:13 +0530

    Floating action Button Design

--- app/app/src/main/java/com/example/timemachine/taskmanagement/HomeActivity.java
+++ app/app/src/main/java/com/example/timemachine/taskmanagement/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.example.timemachine.taskmanagement;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,14 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_home);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Taskmanager.io");
+
+        fab = findViewById(R.id.fab_btn);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "add new task", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
