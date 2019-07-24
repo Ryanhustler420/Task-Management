8c6143d8cb8526a5973b4947c37551813708b9b1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-24 13:22:52 +0530

    Design Registration Layout

--- app/app/src/main/java/com/example/timemachine/taskmanagement/MainActivity.java
+++ app/app/src/main/java/com/example/timemachine/taskmanagement/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.timemachine.taskmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginbtn;
+    TextView signup_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginbtn = findViewById(R.id.login_btn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // do something when login button is pressed
+            }
+        });
+
+        signup_text = findViewById(R.id.signup_text);
+        signup_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(signup);
+            }
+        });
     }
 }
