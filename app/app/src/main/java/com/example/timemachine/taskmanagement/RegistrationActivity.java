027611bf7a5dba699e594ef70a1f033d5ce52382
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-24 13:30:46 +0530

    Connect Registration Activity

--- app/app/src/main/java/com/example/timemachine/taskmanagement/RegistrationActivity.java
+++ app/app/src/main/java/com/example/timemachine/taskmanagement/RegistrationActivity.java
@@ -1,13 +1,43 @@
 package com.example.timemachine.taskmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText username, email, password;
+    private Button btnReg;
+    private TextView loginTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        username = findViewById(R.id.username_signup);
+        email = findViewById(R.id.email_signup);
+        password = findViewById(R.id.password_signup);
+
+        btnReg = findViewById(R.id.signup_btn);
+        loginTxt = findViewById(R.id.login_text);
+
+        loginTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // do something when user clicks Register Button
+            }
+        });
     }
 }
