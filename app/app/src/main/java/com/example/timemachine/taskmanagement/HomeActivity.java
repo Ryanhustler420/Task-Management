d7ba771338fd9db85aa87e70f5804a47cb38c5e2
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-25 12:38:40 +0530

    Connect Input Field Part-14

--- app/app/src/main/java/com/example/timemachine/taskmanagement/HomeActivity.java
+++ app/app/src/main/java/com/example/timemachine/taskmanagement/HomeActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -33,6 +36,33 @@ public class HomeActivity extends AppCompatActivity {
             View v = inflater.inflate(R.layout.custom_input_field, null);
             myDialog.setView(v);
             AlertDialog dialog = myDialog.create();
+
+            final EditText title = v.findViewById(R.id.edt_title);
+            final EditText note = v.findViewById(R.id.edt_note);
+
+            Button btnSave = v.findViewById(R.id.btn_save);
+
+            btnSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String mTitle = title.getText().toString().trim();
+                    String mNote = note.getText().toString().trim();
+
+                    if(TextUtils.isEmpty(mTitle)) {
+                        title.setError("Add Title");
+                        return;
+                    }
+
+                    if(TextUtils.isEmpty(mNote)){
+                        note.setError("Add Description");
+                        return;
+                    }
+
+                    // Field Checked Successfully Not Perform Fire base actions
+
+                }
+            });
+
             dialog.show();
             }
         });
